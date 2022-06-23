b518315b929b7097752f755908ceec6ed9805aca
Abdullah Jamiu <95505963+roji-tech@users.noreply.github.com>
2022-06-23 17:49:54 +0100

    Update Login.jsx

--- src/components/auth/Login.jsx
+++ src/components/auth/Login.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+// import { useState } from 'react'
 import { Link } from "react-router-dom";
 import Button from "./addons/Button";
 import Social from "./addons/Social";
